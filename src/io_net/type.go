2e4e3c718612019e3c201a3d37d7ca4cdeebe0c1
ed006949 <1711185+ed006949@users.noreply.github.com>
2025-06-03 23:09:52 +0300

    feat(io_net/type.go): add OpenVPN, CiscoVPN, and InterimHost structs to support different VPN types refactor(io_net/type.go): import net/netip package for handling IP addresses and ports

--- src/io_net/type.go
+++ src/io_net/type.go
@@ -1,7 +1,23 @@
 package io_net
 
 import (
+	"net/netip"
 	"net/url"
+
+	"rmm23/src/io_crypto"
 )
 
 type URL struct{ *url.URL }
+
+type OpenVPN struct {
+	Listen   []netip.AddrPort `xml:"listen,omitempty"`
+	Hostname []string         `xml:"hostname,omitempty"`
+}
+type CiscoVPN struct {
+	Listen   []netip.AddrPort `xml:"listen,omitempty"`
+	Hostname []string         `xml:"hostname,omitempty"`
+}
+type InterimHost struct {
+	Hostname []string `xml:"hostname,omitempty"`
+	Auth     io_crypto.AuthDB
+}
