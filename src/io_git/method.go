57702cf5c8ae18004d9192c8fe65483d671e26c3
ed006949 <1711185+ed006949@users.noreply.github.com>
2025-06-03 23:08:26 +0300

    refactor(io_git/method.go): remove unused imports and functions related to SSH and HTTP authentication methods chore(io_git/method.go): clean up code by removing redundant error handling and simplifying logic

--- src/io_git/method.go
+++ src/io_git/method.go
@@ -10,8 +10,6 @@ import (
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/plumbing/transport"
-	"github.com/go-git/go-git/v5/plumbing/transport/http"
-	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 
 	"rmm23/src/l"
 )
@@ -118,44 +116,4 @@ func (r *GitDB) Commit(msg string) (err error) {
 	}
 
 	return
-}
-
-func (r *AuthDB) WriteSSH(name string, user string, pemBytes []byte, password string) (err error) {
-	switch _, ok := (*r)[name]; {
-	case ok:
-		return l.EDUPDATA
-	}
-
-	var (
-		sshPublicKeys *ssh.PublicKeys
-	)
-	switch sshPublicKeys, err = ssh.NewPublicKeys(user, pemBytes, password); {
-	case err != nil:
-		return
-	default:
-		(*r)[name] = sshPublicKeys
-		return
-	}
-}
-
-func (r *AuthDB) WriteToken(name string, user string, tokenBytes []byte) (err error) {
-	switch _, ok := (*r)[name]; {
-	case ok:
-		return l.EDUPDATA
-	}
-
-	(*r)[name] = &http.BasicAuth{
-		Username: user,
-		Password: string(tokenBytes),
-	}
-	return
-}
-
-func (r *AuthDB) ReadAuth(name string) (outbound transport.AuthMethod, err error) {
-	switch value, ok := (*r)[name]; {
-	case !ok:
-		return nil, l.ENOTFOUND
-	default:
-		return value, nil
-	}
 }
