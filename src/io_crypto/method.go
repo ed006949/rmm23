48318849a07a49162bbc2efc5c0771ed14708087
ed006949 <1711185+ed006949@users.noreply.github.com>
2025-06-03 23:07:46 +0300

    feat(io_crypto/method.go): add support for writing SSH and token authentication methods to AuthDB refactor(io_crypto/method.go): extract common logic into separate functions for better readability and maintainability chore(io_crypto/method.go): update imports and organize code structure

--- src/io_crypto/method.go
+++ src/io_crypto/method.go
@@ -1,1 +1,49 @@
 package io_crypto
+
+import (
+	"github.com/go-git/go-git/v5/plumbing/transport"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+
+	"rmm23/src/l"
+)
+
+func (r *AuthDB) WriteSSH(name string, user string, pemBytes []byte, password string) (err error) {
+	switch _, ok := (*r)[name]; {
+	case ok:
+		return l.EDUPDATA
+	}
+
+	var (
+		sshPublicKeys *ssh.PublicKeys
+	)
+	switch sshPublicKeys, err = ssh.NewPublicKeys(user, pemBytes, password); {
+	case err != nil:
+		return
+	default:
+		(*r)[name] = sshPublicKeys
+		return
+	}
+}
+
+func (r *AuthDB) WriteToken(name string, user string, tokenBytes []byte) (err error) {
+	switch _, ok := (*r)[name]; {
+	case ok:
+		return l.EDUPDATA
+	}
+
+	(*r)[name] = &http.BasicAuth{
+		Username: user,
+		Password: string(tokenBytes),
+	}
+	return
+}
+
+func (r *AuthDB) ReadAuth(name string) (outbound transport.AuthMethod, err error) {
+	switch value, ok := (*r)[name]; {
+	case !ok:
+		return nil, l.ENOTFOUND
+	default:
+		return value, nil
+	}
+}
