b4603438283b5faae40628dd9c1e6a6af018b7c4
ed006949 <1711185+ed006949@users.noreply.github.com>
2025-06-05 15:43:49 +0300

    refactor(type.go): update package imports and variable names to use 'mod_' prefix for better modularity and clarity

--- src/type.go
+++ src/type.go
@@ -3,16 +3,16 @@ package main
 import (
 	"encoding/xml"
 
-	"rmm23/src/io_cgp"
-	"rmm23/src/io_crypto"
-	"rmm23/src/io_ldap"
 	"rmm23/src/l"
+	"rmm23/src/mod_cgp"
+	"rmm23/src/mod_crypto"
+	"rmm23/src/mod_ldap"
 )
 
 type xmlConf struct {
 	XMLName     xml.Name              `xml:"conf"`
 	Daemon      *l.ControlType        `xml:"daemon,omitempty"`
-	LDAP        *io_ldap.Conf         `xml:"ldap,omitempty"`
+	LDAP        *mod_ldap.Conf        `xml:"ldap,omitempty"`
 	ACMEClients []*xmlConfACMEClients `xml:"acme-clients>acme-client,omitempty"`
 	CGPs        []*xmlConfCGPs        `xml:"cgps>cgp,omitempty"`
 	LEConfMap   map[string]*leConf    `xml:"-"`
@@ -31,17 +31,17 @@ type xmlConfACMEClients struct {
 }
 
 type xmlConfCGPs struct {
-	*io_cgp.Token
+	*mod_cgp.Token
 	Domains map[string][]string `xml:"-"`
 }
 
 type leConf struct {
-	LEDomain            string                 `ini:"Le_Domain"`            // _cdomain="$1"
-	LEAlt               []string               `ini:"Le_Alt" delim:","`     //
-	LERealKeyPath       string                 `ini:"Le_RealKeyPath"`       // _ckey="$2"
-	LERealCertPath      string                 `ini:"Le_RealCertPath"`      // _ccert="$3"
-	LERealCACertPath    string                 `ini:"Le_RealCACertPath"`    // _cca="$4"
-	LERealFullChainPath string                 `ini:"Le_RealFullChainPath"` // _cfullchain="$5"
-	Certificate         *io_crypto.Certificate `ini:"-"`                    //
-	MXList              []string               `ini:"-"`                    //
+	LEDomain            string                  `ini:"Le_Domain"`            // _cdomain="$1"
+	LEAlt               []string                `ini:"Le_Alt" delim:","`     //
+	LERealKeyPath       string                  `ini:"Le_RealKeyPath"`       // _ckey="$2"
+	LERealCertPath      string                  `ini:"Le_RealCertPath"`      // _ccert="$3"
+	LERealCACertPath    string                  `ini:"Le_RealCACertPath"`    // _cca="$4"
+	LERealFullChainPath string                  `ini:"Le_RealFullChainPath"` // _cfullchain="$5"
+	Certificate         *mod_crypto.Certificate `ini:"-"`                    //
+	MXList              []string                `ini:"-"`                    //
 }
