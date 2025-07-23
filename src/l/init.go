087b6dd7a71991db744ac18614f9b79449821767
ed006949 <1711185+ed006949@users.noreply.github.com>
2025-07-23 20:32:38 +0300

    fix(l): Correct flag function assignments and reorder
    Corrected the function assigned to daemonVerbosity flag. Reordered flag function assignments for daemonConfig and daemonDB to align with their respective setter functions.

--- src/l/init.go
+++ src/l/init.go
@@ -12,12 +12,12 @@ func Initialize() {
 
 	Run.verbositySet(Run.verbosity)
 
-	flag.Func(daemonFlagName[daemonVerbosity], daemonEnvDescription[daemonVerbosity], Run.verbositySetString)
+	flag.Func(daemonFlagName[daemonConfig], daemonEnvDescription[daemonConfig], Run.configSetString)
+	flag.Func(daemonFlagName[daemonDB], daemonEnvDescription[daemonDB], Run.dbSetString)
 	flag.Func(daemonFlagName[daemonDryRun], daemonEnvDescription[daemonDryRun], Run.dryRunSetString)
 	flag.Func(daemonFlagName[daemonMode], daemonEnvDescription[daemonMode], Run.modeSetString)
 	flag.Func(daemonFlagName[daemonNode], daemonEnvDescription[daemonNode], Run.nodeSetString)
-	flag.Func(daemonFlagName[daemonDB], daemonEnvDescription[daemonDB], Run.dbSetString)
-	flag.Func(daemonFlagName[daemonConfig], daemonEnvDescription[daemonConfig], Run.configSetString)
+	flag.Func(daemonFlagName[daemonVerbosity], daemonEnvDescription[daemonVerbosity], Run.verbositySetString)
 
 	flag.Parse()
 }
