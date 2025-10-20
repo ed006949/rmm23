cbb4e2d132b53c984507da97abfe72abe690d16f
ed006949 <1711185+ed006949@users.noreply.github.com>
2025-10-20 10:44:07 +0300

    refactor(l): Comment out logging functions

--- src/l/func.go
+++ src/l/func.go
@@ -2,22 +2,20 @@ package l
 
 import (
 	"strings"
-
-	"github.com/rs/zerolog/log"
 )
 
-func Emergency(e Z)     { log.Fatal().EmbedObject(e).Send() }
-func Alert(e Z)         { log.Fatal().EmbedObject(e).Send() }
-func Critical(e Z)      { log.Fatal().EmbedObject(e).Send() }
-func Error(e Z)         { log.Error().EmbedObject(e).Send() }
-func Warning(e Z)       { log.Warn().EmbedObject(e).Send() }
-func Notice(e Z)        { log.Info().EmbedObject(e).Send() }
-func Informational(e Z) { log.Info().EmbedObject(e).Send() }
-func Debug(e Z)         { log.Debug().EmbedObject(e).Send() }
-func Trace(e Z)         { log.Trace().EmbedObject(e).Send() }
-func Panic(e Z)         { log.Panic().EmbedObject(e).Send() }
-func Quiet(e Z)         {}
-func Disabled(e Z)      {}
+// func Emergency(e Z)     { log.Fatal().EmbedObject(e).Send() }
+// func Alert(e Z)         { log.Fatal().EmbedObject(e).Send() }
+// func Critical(e Z)      { log.Fatal().EmbedObject(e).Send() }
+// func Error(e Z)         { log.Error().EmbedObject(e).Send() }
+// func Warning(e Z)       { log.Warn().EmbedObject(e).Send() }
+// func Notice(e Z)        { log.Info().EmbedObject(e).Send() }
+// func Informational(e Z) { log.Info().EmbedObject(e).Send() }
+// func Debug(e Z)         { log.Debug().EmbedObject(e).Send() }
+// func Trace(e Z)         { log.Trace().EmbedObject(e).Send() }
+// func Panic(e Z)         { log.Panic().EmbedObject(e).Send() }
+// func Quiet(e Z)         {}
+// func Disabled(e Z)      {}
 
 // envName generates an environment variable name based on the daemon name and a daemon flag name.
 // It converts the combined name to uppercase and replaces hyphens with underscores.
