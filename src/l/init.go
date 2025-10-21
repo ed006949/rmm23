d25e324f345e7fb7352bb5238d69c44c15fb9384
ed006949 <1711185+ed006949@users.noreply.github.com>
2025-10-21 15:33:49 +0300

    refactor(log): clarify zerolog caller skip frame count

--- src/l/init.go
+++ src/l/init.go
@@ -7,8 +7,7 @@ import (
 )
 
 func Initialize() {
-	// l.log function call nesting depth is 1+1
-	zerolog.CallerSkipFrameCount = zerolog.CallerSkipFrameCount + 1 + 1
+	zerolog.CallerSkipFrameCount = zerolog.CallerSkipFrameCount + 1 /* level method */ + 1 /* log method */
 
 	Run.verbositySet(Run.verbosity)
 
